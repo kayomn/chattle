e95e2e984dc939a9fdcc487b158c7dee08581b4b
Kayomn <kayomn@kayomn.net>
2021-09-30 16:47:26 +0100

    Fix server hang

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ if (__name__ == "__main__"):
         # Avoid bind() exception: OSError: [Errno 48] Address already in use
         client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         client_socket.bind((config.host, config.port))
-        client_socket.listen()
+        client_socket.listen(100)
 
         clients = []
 
@@ -19,26 +19,30 @@ if (__name__ == "__main__"):
             client_connection.send("Welcome to this chatroom!".encode("utf-8"))
 
             try:
+                print("Waiting for data")
                 data = client_connection.recv(4096)
 
-                print("test", data if data else "OOOPS")
-
                 while data:
-                    message = "<" + client_address[0] + "> " + data
+                    message = f"<{client_address[0]}> {data}"
 
                     print(message)
+                    print("Propagating to clients")
 
                     for client in clients:
                         if client != client_connection:
                             try:
-                                client.send(message)
+                                client.send(message.encode("utf-8"))
 
                             except:
                                 client.close()
+
                                 # if the link is broken, we remove the client
                                 if client_connection in clients:
                                     clients.remove(client_connection)
 
+                    print("Finished propagating")
+
+                    print("Waiting for data")
                     data = client_connection.recv(4096)
 
                 if client_connection in clients:
